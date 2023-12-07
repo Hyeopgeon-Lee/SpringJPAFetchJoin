b53ddf47978e3b1376f91493ab11238c6d7c50fd
Hyeopgeon Lee <39357722+Hyeopgeon-Lee@users.noreply.github.com>
2023-12-07 09:54:15 +0900

    설명추가

--- src/main/java/kopo/poly/controller/NoticeController.java
+++ src/main/java/kopo/poly/controller/NoticeController.java
@@ -56,9 +56,12 @@ public class NoticeController {
 
         // 공지사항 리스트 조회하기
         // Java 8부터 제공되는 Optional 활용하여 NPE(Null Pointer Exception) 처리
+
+        // JPA의 JPQL 활용한 Fetch Join 예
 //        List<NoticeDTO> rList = Optional.ofNullable(noticeService.getNoticeList())
 //                .orElseGet(ArrayList::new);
 
+        // QueryDSL 활용한 Fetch Join 예
         List<NoticeDTO> rList = Optional.ofNullable(noticeService.getNoticeListForQueryDSL())
                 .orElseGet(ArrayList::new);
 
